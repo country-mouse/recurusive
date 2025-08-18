cd4d2ae449ee6822764df951d660ca9af92906e6
Jeon Jeongrim <jrjeon1115@gmail.com>
2025-08-18 18:16:03 +0900

    recur using memo p185

--- recrusive_test.c
+++ recrusive_test.c
@@ -1,21 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+static char memo[128][1024];
+
 void recur(int n)
 {
-	if (n > 0)
+	if (memo[n + 1][0] != '\0')
+		printf("%s", memo[n + 1]);
+	else
 	{
-		recur(n - 1);
-		printf("%d\n", n);
-		recur(n - 2);
+		if (n > 0)
+		{
+			recur(n - 1);
+			printf("%d\n", n);
+			recur(n - 2);
+			sprintf(memo[n + 1], "%s%d\n%s", memo[n], n, memo[n - 1]);
+		}
+		else
+			strcpy(memo[n + 1], "");
 	}
 }
 
 int main(void)
 {
 	int x;
-	printf("enter number: ");
+	printf("enter any number: ");
 	scanf("%d", &x);
+
 	recur(x);
 
 	return 0;
